45386a98d57b41fcb447276d7c0b6820de4a11b8
crscillitoe <crscillitoe@wpi.edu>
2019-11-15 20:01:29 -0500

    Added BuildObjectWithSpecificErrorMessage
    Added BuildObjectWithSpecificErrorMessage and corresponding test
    to verify that the CannotDefineOddErrorFactory can build errors
    with a specified message that can later be retrieved for debugging
    purposes.

--- Lib/Implementation/Core/Classes/Factories/Errors/CannotDefineOddErrorFactory.spec.ts
+++ Lib/Implementation/Core/Classes/Factories/Errors/CannotDefineOddErrorFactory.spec.ts
@@ -1,7 +1,22 @@
 import { CannotDefineOddErrorFactory } from "./CannotDefineOddErrorFactory";
 import { CannotDefineOddError } from "../../Errors/CannotDefineOddError";
 
-test("CannotDefineOddErrorFactory builds CannotDefineOddError", () => {
+test("CannotDefineOddErrorFactory builds CannotDefineOddError", async () => {
   const Factory = new CannotDefineOddErrorFactory();
-  expect(Factory.BuildObject()).toBeInstanceOf(CannotDefineOddError);
+  const ResolvedError = await Factory.BuildObject();
+  expect(ResolvedError).toBeInstanceOf(CannotDefineOddError);
 });
+
+test(
+  "CannotDefineOddErrorFactory builds CannotDefineOddError " +
+    "with specific error message when asked",
+  async () => {
+    const SpecificErrorMessageForVerification = "Specific error message";
+    const Factory = new CannotDefineOddErrorFactory();
+    const ResolvedError = await Factory.BuildObjectWithSpecificErrorMessage(
+      SpecificErrorMessageForVerification
+    );
+    expect(ResolvedError).toBeInstanceOf(CannotDefineOddError);
+    expect(ResolvedError.message).toBe(SpecificErrorMessageForVerification);
+  }
+);
--- Lib/Implementation/Core/Classes/Factories/Errors/CannotDefineOddErrorFactory.ts
+++ Lib/Implementation/Core/Classes/Factories/Errors/CannotDefineOddErrorFactory.ts
@@ -4,7 +4,13 @@ import { CannotDefineOddError } from "../../Errors/CannotDefineOddError";
 export class CannotDefineOddErrorFactory
   implements EnterpriseOddVerifierFactoryInterface<CannotDefineOddError> {
   BuildObject(): Promise<CannotDefineOddError> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve(new CannotDefineOddError());
+  }
+
+  BuildObjectWithSpecificErrorMessage(
+    SpecificErrorMessage: string
+  ): Promise<CannotDefineOddError> {
+    return Promise.resolve(new CannotDefineOddError(SpecificErrorMessage));
   }
 
   DestroyObject(ToDestroy: CannotDefineOddError): Promise<boolean> {
